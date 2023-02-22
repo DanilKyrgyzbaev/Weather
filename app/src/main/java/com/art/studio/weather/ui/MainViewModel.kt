ead1e5a039a725172c238c7d7eab556103b427f4
Danil Kyrgyzbaev <daniel-nurai@mail.rubranchname>
2023-02-22 07:12:28 +0600

    fix Hide & Protect API Keys

--- app/src/main/java/com/art/studio/weather/ui/MainViewModel.kt
+++ app/src/main/java/com/art/studio/weather/ui/MainViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.art.studio.weather.BuildConfig
 import com.art.studio.weather.data.api.model.HourlyForecast
 import com.art.studio.weather.domain.usecases.GetAllWeatherUseCase
 import com.art.studio.weather.utils.ResultStatus
@@ -24,7 +25,7 @@ class MainViewModel @Inject constructor(
     }
     fun getAllWeather() {
         val locationKey = "222844"
-        val apiKey = "IV8wVzv9Gm9EhgdG62L3fQVtyoUkV5CI"
+        val apiKey = BuildConfig.API_KEY
         val language = "en"
         val details = true
         val metric = true
